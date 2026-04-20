6b077ea05e61a22b75c5d77e76bbd484ca22bdc8
MaykeGMG <maykegmg15@gmail.com>
2026-04-20 11:18:57 -0300

    inicio-classe-menu

--- src/sistema_bancario/menu.java
+++ src/sistema_bancario/menu.java
@@ -1,5 +1,18 @@
 package sistema_bancario;
 
-public class menu {
+import java.util.Scanner;
 
+public class menu {
+	
+	public static void menu() {
+		
+		Scanner leitor = new Scanner(System.in);//declaração de um scner para entrada de dados
+		
+		int opcao = leitor.nextInt();//variavel de escolha da funcionalidade
+		
+		do {
+			
+		} while (opcao != 0);
+	}
+	
 }
